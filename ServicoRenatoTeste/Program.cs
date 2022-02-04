bcf571fe95b1e4ed42bc205d071b3e68d8710435
Renato  Azevedo <renato.lacerda@imaginaction.com.br>
2022-02-04 14:40:31 -0300

    first commit

---
+++ ServicoRenatoTeste/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace ServicoRenatoTeste
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ ServicoRenatoTeste/Service1.Designer.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.IO;
+using System.Threading;
+
+namespace ServicoRenatoTeste
+{
+    partial class Service1
+    {
+        Timer timer;
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            this.ServiceName = "Service1";
+
+            timer = new Timer(new TimerCallback(meuserv), null, 15000, 30000);
+        }
+
+        #endregion
+
+        private static void meuserv(object sender) {
+            StreamWriter vWriter = new StreamWriter(@"c:\testeServico.txt", true);
+            vWriter.WriteLine("Servico Rodando: " + DateTime.Now.ToString());
+            vWriter.Flush();
+            vWriter.Close();
+        }
+    }
+}
